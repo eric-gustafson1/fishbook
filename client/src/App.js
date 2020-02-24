9708389f6f45f0e53f280c6e796de323ac4cb8ba
Eric Gustafson <weuxfya@gmail.com>
2020-02-23 22:46:23 -0700

    begin to stub out components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './pages/Users';
+import NewPlace from './pages/NewPlace';
 
 const App = () => {
   return <Router>
-    <Route path="/">
-      <Users />
-    </Route>
+    <Switch>
 
+      <Route exact path="/">
+        <Users />
+      </Route>
+      <Route exact path="/places/new">
+        <NewPlace />
+      </Route>
+      <Redirect to="/" />
 
-
+    </Switch>
   </Router>
 };
 
--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
+import pic from './EricHeadShot.jpg'
 
 const Users = () => {
-    return <h2>Users works</h2>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Eric Gustafson',
+            image: pic,
+            places: 5
+        }
+    ];
+
+    return <UsersList items={USERS} />
 };
 
 export default Users;
