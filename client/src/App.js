c22b9eb9c26b49726225eb25909a7c71134854c1
Eric Gustafson <weuxfya@gmail.com>
2020-02-23 21:19:01 -0700

    initial commit with some boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Users from './pages/Users';
+
+const App = () => {
+  return <Router>
+    <Route path="/">
+      <Users />
+    </Route>
+
+
+
+  </Router>
+};
+
+export default App;
